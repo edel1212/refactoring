ecc8573fffdea66a00e1a1fa3f5a31e060609bf8
yoo <edel1212@naver.com>
2023-07-24 22:12:41 +0900

    change Method retuen 변수명

---
+++ chapter01/01-1/ex03.js
@@ -0,0 +1,63 @@
+
+{
+    const plays = require('../json/plays.json');
+    const invoices = require('../json/invoices.json');
+
+    /***
+     * 난해한 변수명은 직관적인 이름으로 변경해주자
+     */
+    function amountFor(perf, play){
+        // let thisAmount = 0;   👎
+        let result = 0;       // 👍
+
+        switch(play.type){
+            case 'tragedy': // 비극
+                result = 40_000;
+                if (perf.audience > 30){
+                    result += 1_000 * (perf.audience - 30); 
+                } 
+                break;
+            case 'comedy':  // 희극
+                result = 30_000;
+                if (perf.audience > 20){
+                    result += 10_000 + 500 * (perf.audience - 20);
+                } 
+                result += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }// switch
+
+        return result;
+    }
+
+
+    function statment(invoices, plays){
+        let totalAmount   = 0;
+        let volumeCredits  = 0;
+        let result = `청구 내역(고객명 ${invoices.customer}) \n`;
+        const format = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format;
+
+        for(let perf of invoices.performances){
+
+            const play = plays[perf.playID];
+
+            /** 👉 변경한  Mehtod로 적용 */
+            let thisAmount = amountFor(perf, play);
+
+            volumeCredits += Math.max(perf.audience - 30, 0);
+
+            if("comedy" === play.type) volumeCredits += Math.floor(perf.audience/5);
+
+            result += `${play.name}: ${format(thisAmount / 100)} ${perf.audience}석\n`;
+            totalAmount += thisAmount;
+        }//for 
+        result += `총액 ${format(totalAmount / 100)}\n`;
+        result += `적립 포인트 ${volumeCredits}점\n`;
+
+        return result;
+    }
+
+
+    console.log(statment(invoices, plays));
+}
